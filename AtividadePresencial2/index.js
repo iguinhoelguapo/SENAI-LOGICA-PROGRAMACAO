e140a9b1f488a331ea060b35c87edad3e3dd2f3b
Igor Magro <79229015+iguinhoelguapo@users.noreply.github.com>
2021-09-28 10:14:49 -0300

    Atividades adicionadas
    Atividade Presencial 2
    Atividade Online 2

---
+++ AtividadePresencial2/index.js
@@ -0,0 +1,15 @@
+let alunos = 11;
+
+for(let cont = 0; cont <= alunos; cont++){
+    if (cont == 0){
+        console.log("O número é zero: " + cont)
+
+    }else if(cont % 2 == 0){
+            console.log("O número é par: " + cont)
+            
+        }else{
+            console.log("O número é impar: " + cont)
+        }
+ }
+
+
